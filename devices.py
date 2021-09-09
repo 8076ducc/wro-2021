85ea3f9ad6f40d5789b4289fa1b90ef7608be49b
neohaojun <neohaojun@gmail.com>
2021-09-09 15:49:54 +0800

    update intake individual close and open method

--- devices.py
+++ devices.py
@@ -91,21 +91,11 @@ class Intake:
         self.left_intake.run(-1000)
         self.right_intake.run(-1000)
 
-    def open_left(self):
-        self.left_intake.run(1000)
-        self.right_intake.hold()
+    def open_side(self, motor: Motor):
+        motor.run(1000)
 
-    def open_right(self):
-        self.left_intake.hold()
-        self.right_intake.run(1000)
-
-    def close_left(self):
-        self.left_intake.run(-1000)
-        self.right_intake.hold()
-
-    def close_right(self):
-        self.left_intake.hold()
-        self.right_intake.run(-1000)
+    def close_side(self, motor: Motor):
+        motor.run(-1000)
 
 
 intake = Intake(left_intake, right_intake)
--- main.py
+++ main.py
@@ -331,18 +331,18 @@ def deposit_parked():
     )
     gyro_turn.single_motor_turn(360, 0, 1)
     gyro_straight.move(-800, 360, lambda: left_color_sensor.reflection() > 20)
-    intake.open_left()
+    intake.open_side(left_intake)
     wait(500)
     gyro_straight.move(800, 360, lambda: left_color_sensor.reflection() > 20)
-    intake.close_left()
+    intake.close_side(left_intake)
     left_intake_possessions.update(None, None)
 
     gyro_turn.single_motor_turn(360, 1, 0)
     gyro_straight.move(-800, 360, lambda: left_color_sensor.reflection() > 20)
-    intake.close_left()
+    intake.open_side(right_intake)
     wait(500)
     gyro_straight.move(800, 360, lambda: left_color_sensor.reflection() > 20)
-    intake.close_right()
+    intake.close_side(right_intake)
     right_intake_possessions.update(None, None)
 
     # TODO: figure out how much need to move for deposit
